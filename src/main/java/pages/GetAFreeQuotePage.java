d26539dfcfb06a6f35ed19c1cfb50023ab80d7b6
YaswiniPriya <111261041+YaswiniPriya@users.noreply.github.com>
2026-02-02 21:27:19 +0530

    Update GetAFreeQuotePage.java

--- src/main/java/pages/GetAFreeQuotePage.java
+++ src/main/java/pages/GetAFreeQuotePage.java
@@ -16,7 +16,7 @@ import org.openqa.selenium.support.ui.Select;
 public class GetAFreeQuotePage {
 	private WebDriver driver;
 	
-	@FindBy (xpath="(//a[@href="#"])[1]")
+	@FindBy (xpath="(//a[@href=\"#\"])[1]")
 	WebElement freequote;
 	
 	@FindBy (xpath="//input[@placeholder=\"Your Name\"]")
